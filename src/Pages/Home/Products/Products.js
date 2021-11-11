1001d16861d9d60be44d32b4f9d679913c41923a
hamunna <hamunna@gmail.com>
2021-11-12 00:22:33 +0600

    Fetching Products from Database

--- src/Pages/Home/Products/Products.js
+++ src/Pages/Home/Products/Products.js
@@ -9,7 +9,7 @@ import wave from '../../../images/wave-underline.png';
 const Products = () => {
 	const [products, setProducts] = useState([]);
 	useEffect(() => {
-		fetch('/products.json')
+		fetch('http://localhost:5000/products')
 			.then(res => res.json())
 			.then(data => setProducts(data));
 	}, []);
